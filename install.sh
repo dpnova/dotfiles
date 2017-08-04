a91ad3838af13e4ea7747fe6cefb15b55ae6612f
David Novakovic <dpn@dpn.name>
2017-08-04 21:36:46 +1000

    Start of an installer.

---
+++ install.sh
@@ -0,0 +1,11 @@
+HERE=`pwd`
+mkdir -p ~/.vimrc/colors
+cd ~/.vimrc/colors
+wget https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim
+ln -s `pwd`/vimrc ~/.vimrc
+cd ~/Downloads
+wget https://github.com/BurntSushi/ripgrep/releases/download/0.5.2/ripgrep-0.5.2-x86_64-unknown-linux-musl.tar.gz
+tar xvzf ripgrep*
+mkdir ~/bin
+cp ripgrep-0.5.2-x86_64-unknown-linux-musl/rg ~/bin
+
